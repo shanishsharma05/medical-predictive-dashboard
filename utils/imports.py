c12ddd0cfe46945a6739be5424b4dbbfa7c9a9a5
Shanish Sharma <shanishsharma@Mac.hub>
2025-11-17 18:18:29 +1300

    Adding shared imports

---
+++ utils/imports.py
@@ -0,0 +1,11 @@
+# Imports and modules
+
+import numpy as np # Loading and cleaning the data
+import pandas as pd # Numerical calculations
+import sklearn as skl # Machine Learning models and preprocessing 
+import matplotlib.pyplot as plt # Visualisations 
+import seaborn as sns # Visulalisations
+import streamlit as st # Web dashboard
+import shap # Explainability of outputs
+import xgboost as xgb
+import joblib as jl # Model saving and loading
